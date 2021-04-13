6d848281d3a9fe30613bc8a56f09ddd6c20f0867
Edward Kim <tykim@pusan.ac.kr>
2021-04-13 20:10:23 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//https://en.cppreference.com/w/cpp/types/size_t
+typedef size_t size_type;
+int main() {
+    size_type i = 0;
+    std::cout << i - 1 << std::endl;
+    return 0;
+}
